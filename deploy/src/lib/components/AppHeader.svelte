d728c414a3a21ebd9a8298f8c0a55069694d1e49
jkaczman <jkaczman@pm.me>
2026-05-09 13:26:40 -0400

    fix(deploy): add iOS app link to href

--- deploy/src/lib/components/AppHeader.svelte
+++ deploy/src/lib/components/AppHeader.svelte
@@ -85,7 +85,7 @@
     <div class="shared-header__actions">
       <a
         class="shared-header__store shared-header__store--ios"
-        href="https://apps.apple.com/app/id0000000000"
+        href="https://apps.apple.com/us/app/secluso/id6756543429"
         on:click|preventDefault={() => openExternal("https://apps.apple.com/us/app/secluso/id6756543429")}
       >
         <img src="/deploy-assets/header-ios-latest.svg" alt="" />
